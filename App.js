51ad94c307f8f3b253295e007ffb9b7a18547d0f
dragon5354 <dragon5354@naver.com>
2021-03-22 13:17:08 +0900

    Coding the UI part Three

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Dimensions, Platform } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import ToDo from "./ToDo"
 
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
   render() {
+    const { newToDo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar style="light" />
         <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"}/>
+          <TextInput 
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._crontollNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            <ToDo />
+          </ScrollView>
         </View>
       </View>
     );
   }
+  _crontollNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -45,5 +66,14 @@ const styles = StyleSheet.create({
         elevation : 3
       }
     })
+  },
+  input: {
+    padding :20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize:25
+  },
+  toDos: {
+    alignItems: "center"
   }
 });
